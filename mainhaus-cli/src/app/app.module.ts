1f3b929a4d6b76f92de76368f0469719e8677939
mainhaus <sajo_sj@hotmail.com>
2017-10-14 16:44:30 +0200

    vahid

--- mainhaus-cli/src/app/app.module.ts
+++ mainhaus-cli/src/app/app.module.ts
@@ -5,18 +5,21 @@ import { RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
 import { PocetnaComponent } from './pocetna/pocetna.component';
 import { ONamaComponent } from './o-nama/o-nama.component';
+import { ReferenceComponent } from './reference/reference.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PocetnaComponent,
-    ONamaComponent
+    ONamaComponent,
+    ReferenceComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path:'pocetna',component:PocetnaComponent},
       {path:'o-nama',component:ONamaComponent},
+      {path:'reference',component:ReferenceComponent},
     ])
   ],
   providers: [],
