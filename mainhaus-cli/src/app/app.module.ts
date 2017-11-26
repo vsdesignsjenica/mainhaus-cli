fc8db1ab4565b8a21d656fc89e6ba0ceba0a8d4b
mainhaus <sajo_sj@hotmail.com>
2017-11-26 13:41:26 +0100

    va

--- mainhaus-cli/src/app/app.module.ts
+++ mainhaus-cli/src/app/app.module.ts
@@ -6,18 +6,21 @@ import { AppComponent } from './app.component';
 import { PocetnaComponent } from './pocetna/pocetna.component';
 import { ONamaComponent } from './o-nama/o-nama.component';
 import { ReferenceComponent } from './reference/reference.component';
+import { KolekcijeComponent } from './kolekcije/kolekcije.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PocetnaComponent,
     ONamaComponent,
-    ReferenceComponent
+    ReferenceComponent,
+    KolekcijeComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path:'pocetna',component:PocetnaComponent},
+      {path:'kolekcije',component:KolekcijeComponent},
       {path:'o-nama',component:ONamaComponent},
       {path:'reference',component:ReferenceComponent},
     ])
